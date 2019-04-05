2b0aad347a062084d6801920a0f1083ae3c944a8
franco1999 <36521789+franco1999@users.noreply.github.com>
2019-04-05 12:20:49 -0300

    P1 5/04/19

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char nombre[6];
+    char otroNombre[50]="Jose Perez"
+    int compara;
+
+    //strcpy(nombre,otroNombre);
+    //strncpy(nombre,otroNombre,5);
+    printf("Su nombre es: %s", nombre);
+    //fgets(nombre, 48, stdin);           //50=>cantidad de caracteres que lee
+    scanf("%[^\n]", nombre);
+    //gets(nombre);
+    //largo = strlen(nombre);
+    //printf("%d", largo);
+
+    compara = strcmp(nombre, "pepe");
+    printf("%d", compara);
+
+    return 0;
+}
