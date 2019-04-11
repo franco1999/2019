55067c79c53d2136e0afb81abe765a1c8eb3c4d2
franco1999 <36521789+franco1999@users.noreply.github.com>
2019-04-11 12:21:36 -0300

    L1 11/04/19

--- main.c
+++ main.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-int main()
+#define CANTIDAD_ALUMNOS 4
+
+/*int main()
 {
-    char nombre[6];
-    char otroNombre[50]="Jose Perez"
-    int compara;
+    char nombres[CANTIDAD_ALUMNOS][50]= {"Alfredo", "Rogelio", "Julieta", "Pablo"};
+    char apellidos[CANTIDAD_ALUMNOS][50]={"Mercurio", "Agua", "Roberto", "McCartney"};
+    int edades[CANTIDAD_ALUMNOS]={54,24,37,18};
+    int notas[CANTIDAD_ALUMNOS]={2,4,7,10};
+    int i;
 
-    //strcpy(nombre,otroNombre);
-    //strncpy(nombre,otroNombre,5);
-    printf("Su nombre es: %s", nombre);
-    //fgets(nombre, 48, stdin);           //50=>cantidad de caracteres que lee
-    scanf("%[^\n]", nombre);
-    //gets(nombre);
-    //largo = strlen(nombre);
-    //printf("%d", largo);
+     for(i=0; i<CANTIDAD_ALUMNOS; i++)
+        {
+            if(nombres[i] != -1)
+                {
+                    printf("\n nombre:%s\n apellido: %s", nombres[i], apellidos[i]);
+                }
+        }
 
-    compara = strcmp(nombre, "pepe");
-    printf("%d", compara);
+    char unNombre[50];
+    getString("\n\nIngrese nombre:", &unNombre);
+    //printf("el nombre es :%s", unNombre);
+    strcpy(nombres[0], unNombre);
 
-    return 0;
+    for(i=0; i< CANTIDAD_ALUMNOS; i++)
+        {
+            getString("\n\nIngrese nombre:", nombres[1]);
+        }
+    for(i=0; i< CANTIDAD_ALUMNOS; i++)
+        {
+            printf("\n nombre:%s apellido:%s nota:%d edad:%d",nombres[1], apellidos[1], notas [1], edades[1]);
+        }
 }
+*/
+getStringLetras(mensaje, imput)
+{
+    char aux[50];
+    getString(mensaje, aux);
+    int esSoloLetras(char palabra []);
+}
+
+
+
+int 1;
+
+    for(i=1; )
+
+    if(palabra[i]!=' ')
+    {
+        if(palabra[i]<'a'&&palabra[i]>'z')
+            {
+                if(palabra[i]<'A'&& palabra[i]>'z')
+                return 0;
+            }
+    }
+}
+
+
+
+return 1;
