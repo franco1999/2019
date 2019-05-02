c45277a23af483f38e4bc150c53a665f28910d0b
franco1999 <36521789+franco1999@users.noreply.github.com>
2019-05-02 10:53:06 -0300

    Laboratorio 02/05/19

---
+++ biblioteca.h
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LIBRE 0
+#define OCUPADO 1
+#define BORRADO -1
+
+    typedef struct
+
+    void inicializarProductos(eProducto listaProductos[],int tam)
+{
+
+    float precio[10]={42,43, 44, 40, 41, 42, 35, 36, 37, 38};
+    char IdProveedor[10][50]= {"1", "2", "3", "1", "2", "3", "1", "2", "3", "1"};
+    char codigoDeBarra[10][50]={"111","222","333","444","555","666","777","888","999","101"};
+    char fechaDeVencimiento[10][50]={"01/01/2020","01/01/2020","01/01/2020","01/01/2020","01/01/2020","01/01/2020","01/01/2020","01/01/2020","01/01/2020","01/01/2020"};
+    char nombre[10][50]= {"Infinia" "AxionPower" "Vpower" "InfiniaDiesel" "AxionDiesel" "VpowerDiesel" "Super" "AxionSuper" "Normal" "Kerosene"};
+
+    for(i=0; i<10; i++)
+        {
+            printf();
+
+
+        }
+
+}eProducto;
+
+
+/*eProducto pedirProducto();
+
+void inicializarProductos(eProducto[],int);//OK
+
+void mostrarProducto(eProducto);
+
+void cargarArray(eProducto[], int);
+
+void mostrarArray(eProducto[], int);
+
+void construirArray(eProducto[], int);
+
+int insertarProducto(eProducto [], int );
+
+int dameLugarLibre(eProducto[],int);
+
+int existeProducto(eProducto,eProducto[]);
+
+int borrarProducto(eProducto[], int);
+
+int editarProducto(eProducto [], int ); */
+
